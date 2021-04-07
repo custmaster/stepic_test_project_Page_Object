8ba9eb165f2bde6708e23b76be65175795509801
custmaster <custmaster.com>
2021-04-07 15:31:02 +0300

    убрал повторяющийся метод, лишний

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,6 +9,3 @@ class MainPage(BasePage):
         login_link.click()
     def should_be_login_link(self):
     	assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-    def go_to_login_page(self):
-    	link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-    	link.click() 
