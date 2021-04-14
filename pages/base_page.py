2d7a3a8fef4baa4f9a5fd91a16790f24fce686ae
custmaster <custmaster@gmail.com>
2021-04-14 21:28:07 +0300

    Тест возможности перехода на страницу логина, вынесен в BasePage, т.к. он повторяется для всех страниц сайта. Соответствующие тесты добавлены для product_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import TimeoutException
 import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -11,6 +12,11 @@ class BasePage():
 		self.browser = browser
 		self.url = url
 		# self.browser.implicitly_wait(timeout)
+	def go_to_login_page(self):
+		login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+		login_link.click()
+	def should_be_login_link(self):
+		assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 	def open(self):
 		self.browser.get(self.url)
 	def is_element_present(self, how, what):
@@ -25,7 +31,7 @@ class BasePage():
 		except TimeoutException:
 			return True
 		return False
-	def is_disappeared(self, how, what, timeout=5):
+	def is_disappeared(self, how, what, timeout=4):
 		try:
 			WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
 		except TimeoutException:
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from .pages.product_page import ProductPage
 import time
 import pytest
 from .pages.locators import ProductPageLocators
-
+from .pages.login_page import LoginPage
 
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -24,6 +24,7 @@ from .pages.locators import ProductPageLocators
 # 	page.check_nane_product_in_massage_add_to_basket()
 # 	page.check_price_product_in_massage_add_to_basket()
 
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -33,16 +34,35 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 	page.solve_quiz_and_get_code()
 	assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
-    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+    "Success message is presented, but should not be"
 
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), 'Sucsess messege isnt disappeared'
+    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+    'Sucsess messege isnt disappeared'
+
+@pytest.mark.skip
+def test_guest_should_see_login_link_on_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.go_to_login_page()
+	login_page = LoginPage(browser, browser.current_url)
+	login_page.should_be_login_page()
