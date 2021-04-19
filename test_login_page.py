4f41515fdaff5e15816617306dfa76c810214f95
custmaster <custmaster@gmail.com>
2021-04-19 22:03:58 +0300

    Подготовка кода к ревью. Для проверяемых тестов добавлена метка - @pytest.mark.need_review

--- test_login_page.py
+++
@@ -1,8 +0,0 @@
-from .pages.login_page import LoginPage
-
-
-def test_should_be_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.should_be_login_page()          # выполняем метод страницы — переходим на страницу логина
